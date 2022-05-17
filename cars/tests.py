45c4a0695f238ad8b178f85738500907b2c69fb7
kaiokins <godfrydkuba@gmail.com>
2022-05-17 19:38:26 +0200

    Added tests (url, view) to car model

--- cars/tests.py
+++ cars/tests.py
@@ -1,3 +1,18 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse, resolve
+from .views import cars
+from .models import Car
 
 # Create your tests here.
+class carTests(TestCase):
+
+    # urls test
+    def test_urlCars(self):
+        url = reverse('cars')
+        self.assertEquals(resolve(url).func, cars)
+
+    # views test
+    def test_viewCars(self):
+        client = Client()
+        response = client.get(reverse('cars'))
+        self.assertEquals(response.status_code, 200)
--- cars/views.py
+++ cars/views.py
@@ -3,3 +3,4 @@ from django.shortcuts import render
 # Create your views here.
 def cars(request):
     return render(request, 'cars/cars.html')
+
