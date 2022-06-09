bcad8c4ac62020ade451dee79380b13486814daa
kaiokins <godfrydkuba@gmail.com>
2022-06-09 21:13:11 +0200

    Added new env

--- otoauto/settings.py
+++ otoauto/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-x4&cj*4eyh@t)sn!jjqxrme2&%8)fgjnwj7+etj(3*=wtsq8n3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['stormy-sands-92720.herokuapp.com']
+ALLOWED_HOSTS = ['stormy-thicket-26397.herokuapp.com']
 
 
 # Application definition
