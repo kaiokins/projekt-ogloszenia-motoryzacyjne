3023daaa2f6ba32b225752b1ad25bbb265353336
kaiokins <godfrydkuba@gmail.com>
2022-06-09 19:20:23 +0200

    Added CI

--- otoauto/settings.py
+++ otoauto/settings.py
@@ -141,7 +141,7 @@ MESSAGE_TAGS = {
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'otoautocomp2@gmail.com'
-EMAIL_HOST_PASSWORD = ''
+EMAIL_HOST_PASSWORD = 'rrwvyhnuxpdwnmer'
 EMAIL_USE_TLS = True
 
 # Default primary key field type
