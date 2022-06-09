2c222ed0a7ad2dc9e9aad29c5d4b330912bd54a9
kaiokins <66028140+kaiokins@users.noreply.github.com>
2022-06-09 23:51:36 +0200

    Update settings.py

--- otoauto/settings.py
+++ otoauto/settings.py
@@ -144,7 +144,7 @@ MESSAGE_TAGS = {
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'otoautocomp2@gmail.com'
-EMAIL_HOST_PASSWORD = 'rrwvyhnuxpdwnmer'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
 
 # Default primary key field type
