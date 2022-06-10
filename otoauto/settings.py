b1cfe95756ad0c6f3a9ad0a6de41a005c70120ea
kaiokins <godfrydkuba@gmail.com>
2022-06-10 17:34:54 +0200

    Change django.yml

--- otoauto/settings.py
+++ otoauto/settings.py
@@ -81,18 +81,18 @@ WSGI_APPLICATION = 'otoauto.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'otoauto_db',
-#         'USER': 'postgres',
-#         'PASSWORD': '123456',
-#         'HOST': 'localhost',
-#         'PORT': '5432'
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'otoauto_db',
+        'USER': 'postgres',
+        'PASSWORD': '123456',
+        'HOST': 'localhost',
+        'PORT': '5432'
+    }
+}
 
-DATABASES = {'default': dj_database_url.config(default='postgres://postgres:123456@localhost/otoauto_db')}
+# DATABASES = {'default': dj_database_url.config(default='postgres://postgres:123456@localhost/otoauto_db')}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
